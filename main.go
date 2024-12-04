691719c319e27468f1aaebbe709243a9b1342714
Livio Spring <livio.a@gmail.com>
2024-12-04 13:50:47 +0100

    base implementation

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func main() {
+	port := 8080
+	idleTimeout := 5 * time.Second
+	readTimeout := 5 * time.Second
+	writeTimeout := 5 * time.Second
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %v", r.URL.Path)
+	})
+
+	http2Server := &http2.Server{IdleTimeout: idleTimeout}
+	http1Server := &http.Server{Handler: h2c.NewHandler(handler, http2Server), ReadTimeout: readTimeout, IdleTimeout: idleTimeout, WriteTimeout: writeTimeout}
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+	err = http1Server.Serve(lis)
+	if err != nil {
+		panic(err)
+	}
+}
